4d4e1d1f5613e44e4c7a94453667a3c9ad1ef323
robert <robertomarcial688@gmail.com>
2021-11-24 20:17:27 -0600

    Validaciones en categorias terminada con js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 module ApplicationHelper
 
+     def error_message(model, item)
+        error = model.errors[item].first
+        return if error.nil?
+        
+        content_tag(:p, error, class: "is-invalid")
+    end
+
+    def input_error_class(model, item)
+        error = model.errors[item].first
+        return if error.nil? 
+        "is-invalid"
+    end
+
 
 end
