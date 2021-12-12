96a1659faa7059e808282463b6e5868eed42d1d9
robert <robertomarcial688@gmail.com>
2021-12-11 23:10:44 -0600

    Modificacion de archivos

--- app/models/editorial.rb
+++ app/models/editorial.rb
@@ -1,7 +1,7 @@
 class Editorial < ApplicationRecord
 
-    validates :nombre, uniqueness: { message: "ya existe la empresa debe ser unico, favor de cambiar nombre de la empresa" }
-    validates :telefono, uniqueness: { message: "El numero que usted intenta registrar ya existe favor de cambiar" } 
+    validates :nombre, uniqueness: { message: "de la empresa ya existe debe ser unico, favor de cambiar nombre de la empresa" }
+    validates :telefono, uniqueness: { message: "que usted intenta registrar ya existe favor de cambiar" } 
     validates :nombre ,:pais, :telefono, presence:{message: "no debe estar en blanco"}
     validates :telefono, format:{with: /\A\d+\z/, message: "Debe contener unicamente digitos." }
     validate :code_validate
@@ -13,7 +13,7 @@ private
     def code_validate
 
         if self.telefono.nil? || self.telefono.length != 10 
-            self.errors.add(:telefono, 'El numero que usted intenta registrar debe poseer 10 digitos')
+            self.errors.add(:telefono, 'que usted intenta registrar debe poseer 10 digitos')
         end    
 
 
