9c7733319b4e058700e4ef5a1085180075d86d7b
robert <robertomarcial688@gmail.com>
2021-12-14 00:30:47 -0600

    Se agrego la tabla country con sus respectivos registros

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_010005) do
+ActiveRecord::Schema.define(version: 2021_12_14_053344) do
 
   create_table "authors", force: :cascade do |t|
     t.string "nombre"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2021_11_20_010005) do
   create_table "categories", force: :cascade do |t|
     t.string "nombre"
     t.string "descripcion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "pais"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+registros_pais = Country.create([{pais: 'Bielorrusia'},{pais: 'Belice'},{pais: 'Bélgica' },{pais: 'Bermudas'}, {pais: 'Bulgaria'}, {pais: 'Brasil'},
+                                {pais: 'Brasil'}, {pais: 'Reino Unido'}, {pais: 'Hungría'},{pais: 'Vietnam'},{pais: 'Haiti'} ,{pais: 'Guadalupe' } ,{pais: 'Alemania'} ,{pais:'Países Bajos' } ,{pais: 'Grecia'} ,{pais: 'Georgia' },
+                                {pais: 'Dinamarca' } ,{pais: 'Egipto' } ,{pais: 'Israel'} ,{pais: 'India'} ,{pais: 'Irán' } ,{pais: 'Irlanda'} ,{pais: 'España'} ,{pais: 'Italia'} ,{pais: 'Kazajstán'} ,{pais: 'Camerún'} ,{pais: 'Canadá'} ,{pais: 'Chipre'} ,{pais:'Kirguistán' } ,{pais: 'China'},
+                                {pais: 'Costa Rica'} ,{pais:  'Kuwait'} ,{pais: 'Letonia'} ,{pais: 'Libia'} ,{pais: 'Lituania'} ,{pais: 'Luxemburgo'} ,{pais: 'México'} ,{pais: 'Moldavia' } ,{pais: 'Mónaco'} ,{pais: 'Nueva Zelanda'} ,{pais: 'Noruega'} ,{pais:  'Polonia'} ,{pais: 'Portugal'} ,{pais: 'Reunión'},
+                                {pais:'Rusia' } ,{pais:'El Salvador' },{pais: 'Eslovaquia'} ,{pais: 'Eslovenia'},{pais: 'Surinam'} ,{pais: 'Estados Unidos'},{pais: 'Ucrania'} ,{pais:'Francia' } ,{pais:'Suiza' },{pais: 'Bolivia'} ,{pais: 'Panamá'},{pais: 'Sudáfrica'} ,{pais: 'Alemania'} ,{pais: 'Holanda' }, 
+                                {pais: 'Portugal'} ,{pais: 'Guatemala'} ,{pais: 'Irak' } ,{pais: 'Puerto Rico' } ,{pais: 'Trinidad y Tobago' } ,{pais: 'Ruanda' } ,{pais: 'Qatar'},{pais: 'Paraguay'},{pais: 'Venezuela'} ,{pais: 'Uruguay' } ,{pais: 'Yemen'}  
+
+]) 
